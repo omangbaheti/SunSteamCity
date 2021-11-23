4962910da3484c55a211a35b79097b6f20cc822c
omangbaheti <60500898+omangbaheti@users.noreply.github.com>
2021-11-23 21:24:24 +0530

    Final COmmit

--- Assets/Scripts/XR/PlacementManager.cs
+++ Assets/Scripts/XR/PlacementManager.cs
@@ -9,11 +9,6 @@ using UnityEngine.XR.ARSubsystems;
 [RequireComponent(typeof(ARRaycast))]
 public class PlacementManager : MonoBehaviour
 {
-    public TextMeshProUGUI InputTouches;
-    public TextMeshProUGUI PlacementPoseValid;
-    public TextMeshProUGUI SpawnedObject;
-    public TextMeshProUGUI CurrentTime;
-    
     [SerializeField] private GameObject arGameObject;
     [SerializeField] private GameObject placementIndicator;
 
@@ -21,30 +16,19 @@ public class PlacementManager : MonoBehaviour
     private Pose placementPose;
     private bool placementPoseValid = false;
     private GameObject spawnedObject = null;
-
-    public GameObject ARGamerObject
-    {
-        get => arGameObject;
-        set => arGameObject = value;
-    }
-
+    private bool placed = false;
     private void Awake()
     {
-        InputTouches.SetText($"InputTouches:LMAO");
-        PlacementPoseValid.SetText($"PlacementPoseValid:LMAO");
-        SpawnedObject.SetText($"Spawned Object:LMAO");
         arRaycastManager = GetComponent<ARRaycastManager>();
     }
     private void Update()
     {
-        CurrentTime.SetText($"{Time.time}");
-        InputTouches.SetText($"InputTouches:{Input.touchCount > 0}");
-        // PlacementPoseValid.SetText($"PlacementPoseValid:{placementPoseValid}");
-        SpawnedObject.SetText($"Spawned Object:{spawnedObject == null}");  
         if (spawnedObject == null && placementPoseValid && Input.touchCount > 0)
         {
-            ARPlacedObject();
+            ARPlaceObject();
         }
+
+        if (placed) return;
         UpdatePlacementIndicator();
         UpdatePlacementPose();
     }
@@ -55,7 +39,6 @@ public class PlacementManager : MonoBehaviour
         Vector3 screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
         List<ARRaycastHit> hits = new List<ARRaycastHit>();
         arRaycastManager.Raycast(screenCenter, hits, TrackableType.PlaneWithinPolygon);
-        PlacementPoseValid.SetText($"PlacementPoseValid:{hits.Count}");
         placementPoseValid = hits.Count > 0;
         if (placementPoseValid)
         {
@@ -76,8 +59,9 @@ public class PlacementManager : MonoBehaviour
         }
     }
 
-    private void ARPlacedObject()
+    private void ARPlaceObject()
     {
         spawnedObject = Instantiate(arGameObject, placementPose.position - new Vector3(0, -0.1f, 0), placementPose.rotation);
+        placed = true;
     }
 }
